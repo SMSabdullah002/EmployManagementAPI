39d53a9fe0ee172c958a1a73f9e7adaf718eaad2
Muhammad Abdullah Bin Asim <90253127+SMSabdullah002@users.noreply.github.com>
2021-10-05 22:07:25 +0500

    Add files via upload

---
+++ Controllers/EmploysController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EmloyManagement.modal;
+using EmployeeManagement.Modals;
+
+namespace EmployeeManagement.Controllers
+{
+    [Route("api/Employs")]
+    [ApiController]
+    public class EmploysController : ControllerBase
+    {
+        private readonly Appdbcontext _context;
+
+        public EmploysController(Appdbcontext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Employs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employ>>> GetEmploy()
+        {
+            return await _context.Employ.ToListAsync();
+        }
+
+        // GET: api/Employs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employ>> GetEmploy(int id)
+        {
+            var employ = await _context.Employ.FindAsync(id);
+
+            if (employ == null)
+            {
+                return NotFound();
+            }
+
+            return employ;
+        }
+
+        // PUT: api/Employs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmploy(int id, Employ employ)
+        {
+            if (id != employ.EmployID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employ).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Employ>> PostEmploy(Employ employ)
+        {
+            _context.Employ.Add(employ);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmploy", new { id = employ.EmployID }, employ);
+        }
+
+        // DELETE: api/Employs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmploy(int id)
+        {
+            var employ = await _context.Employ.FindAsync(id);
+            if (employ == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employ.Remove(employ);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployExists(int id)
+        {
+            return _context.Employ.Any(e => e.EmployID == id);
+        }
+    }
+}
---
+++ Modals/Appdbcontext.cs
@@ -0,0 +1,65 @@
+﻿using EmloyManagement.modal;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Modals
+{
+    public class Appdbcontext : DbContext
+    {
+        public Appdbcontext(DbContextOptions<Appdbcontext> options)
+       : base(options)
+        {
+        }
+        public DbSet<Employ> Employ { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Employ>().HasData(new Employ
+            {
+                EmployID = 1,
+                FirstName = "John",
+                LastName = "Smith",
+                Email = "John.smith@gmail.com",
+                Gender = Gender.Male,
+
+            });
+
+            modelBuilder.Entity<Employ>().HasData(new Employ
+            {
+                EmployID = 2,
+                FirstName = "Joe",
+                LastName = "Gabrial",
+                Email = "Joe1987@gmail.com",
+                Gender = Gender.Male,
+
+            });
+
+            modelBuilder.Entity<Employ>().HasData(new Employ
+            {
+                EmployID = 3,
+                FirstName = "Will",
+                LastName = "Hamster",
+                Email = "StrangerThings234@gmail.com",
+                Gender = Gender.Male,
+
+            });
+
+            modelBuilder.Entity<Employ>().HasData(new Employ
+            {
+                EmployID = 4,
+                FirstName = "shela",
+                LastName = "Barber",
+                Email = "Shela.Bhabi@gmail.com",
+                Gender = Gender.Female,
+
+            });
+        }
+
+    }
+}
